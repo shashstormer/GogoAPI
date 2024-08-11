7bae567e0d1627c68e1fd260b3858b6395cfc08a
lirena <sakshamkushwaha12776@gmail.com>
2024-08-11 14:17:22 +0530

    chore: updated gogo_url

--- main.py
+++ main.py
@@ -293,7 +293,7 @@ class Session:
         return resp
 
 
-main_url = "https://anitaku.so"
+main_url = "https://anitaku.pe"
 alternate_domains = ["https://gogoanime3.net/", "https://www9.gogoanimes.fi", ]
 recent_url = "https://ajax.gogocdn.net/ajax/page-recent-release.html?page={}&type={}"
 episodes_url = "https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=0&ep_end=10000&id={}"
